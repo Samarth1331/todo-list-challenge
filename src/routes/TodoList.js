770c7e02568264669381adbf4a458cc8d3959874
Samarth1331 <77462226+Samarth1331@users.noreply.github.com>
2022-02-22 11:26:01 -0500

    Add files via upload

--- src/routes/TodoList.js
+++ src/routes/TodoList.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
 
-function Todo() {
-  const [todos, setTodo] = useState([])
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    if(!todo.text || /^\s*$/.test(todo.text)){
+      return;
+    } 
+  
+    const newTodos = [todo,...todos];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id)
+    setTodos(removeArr)
+  }
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
     <div>
-      <h1>TODO List</h1>
-      <ul>
-      </ul>
+      <h1>Hi! What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo}/>
+      <Todo
+       todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}/>
     </div>
-  )
+  );
 }
 
-export default Todo
+export default TodoList;
